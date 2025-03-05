d99df265f011aa0f72ceef1651dd79b99ffb5906
jzells09 <justinzeller2004@gmail.com>
2025-03-04 23:50:50 -0600

    Tasks form now sends information to database
    Still a WIP, but basic data is now sent to the database from user input on front end.
    
    Also added a test file called titleform.html

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Reminder = require('./js/models/reminder')
+const Reminder = require('./js/models/reminder');
+const bodyParser = require('body-parser');
+const { error } = require('console');
 
 
 // connect to mongodb
@@ -10,6 +12,8 @@ const app = express();
 
 // listen for requests
 app.use(express.static(__dirname));
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (request, response) => 
     {
@@ -31,20 +35,49 @@ app.get('/settings.html', (request, response) =>
 
 app.get('/tasks.html', (request, response) => 
     {
+        const title = request.body.title;
+        console.log(title);
         response.sendFile('./tasks.html', { root: __dirname });
-        Reminder.find()
-            .then((result) => 
-            {
-                
-            }).catch((err) => console.log(err));
+        
     }
 )
+
 
 app.get('/calendar.html', (request, response) => 
     {
         response.sendFile('./calendar.html', { root: __dirname });
     }
 )
+
+app.get('/titleform', (request, response) =>
+{
+    response.sendFile('./titleform.html', { root: __dirname });
+
+})
+
+
+app.post('/submit', (request, response) => {
+    const title = request.body.title; // as of right now, when you enter a title in the tasks screen, it will send a 'reminder' to the DB with the title entered
+    if (title) {
+        response.redirect('/tasks.html');
+        console.log(title);
+        const reminder = new Reminder(
+            {
+                title: title,
+                description: 'description here',
+                date:13,
+                time:12
+            }
+        );
+        
+        reminder.save()
+            .then((result) => {
+                console.log(result);
+            }).catch((err) => console.log(err));
+    } else {
+        response.status(400).send("No title received");
+    }
+});
 
 
 
@@ -55,19 +88,7 @@ mongoose.connect(dbURI)
 // mongoose/mongo sandbox routes
 app.get('/add-task', (request,response) =>
     {
-        const reminder = new Reminder(
-            {
-                title: 'new reminder',
-                description: 'description here',
-                date:13,
-                time:12
-            }
-        );
         
-        reminder.save()
-            .then((result) => {
-                response.send(result)
-            }).catch((err) => console.log(err));
     }
 );
 
