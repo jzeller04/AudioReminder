9fba09f3cb2e04ba05345e38da7bd7390721b216
RileyKn <rileykeene91@gmail.com>
2025-04-21 20:55:15 -0500

    Add googleId field to Reminder schema

--- server/models/reminder.js
+++ server/models/reminder.js
@@ -22,6 +22,10 @@ const reminderSchema = new Schema({
     date: {
         type: Date,
         required: true
+    },
+    googleId: {
+        type: String,
+        default: null
     }
 }, {timestamps: true}); // saves the time when reminder is created
 
