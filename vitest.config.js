82375d45bc5fa206b860c2474b4b4b04b6972ccb
marioonofrio <marofrio7@gmail.com>
2025-04-30 13:41:33 -0500

    adds authentication test

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,12 @@
-// vitest.config.js
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node', // or 'jsdom' if you test DOM stuff
-    include: ['tests/**/*.test.js'],
-  },
+    environmentMatchGlobs: [
+      ['**/components/**', 'jsdom'],
+      ['**/api/**', 'node'],
+    ],
+    include: ['tests/**/*.test.{js,jsx}']
+  }
 })
